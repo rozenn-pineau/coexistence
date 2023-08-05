7f1a63a85b91f684544c3f674c23b6afe59c1f32
Pineau <pineau.rozenn@hotmail.fr>
2023-08-05 11:22:16 -0400

    F1 codes

--- Dropbox (GaTech)/BioSci-Ratcliff/Rozenn/2.CoexistenceProject/12-Writing/11-NE&Esubmission/resubmission/submitted_July23/scripts_to-add-GitHub/F1/Main_Figure1.R
+++
@@ -1,9 +0,0 @@
-# This script calls other scripts to make the elements of Figure 1. 
-
-# Change the path to your F1 directory
-setwd("/Users/rpineau3/Dropbox (GaTech)/BioSci-Ratcliff/Rozenn/2.CoexistenceProject/12-Writing/11-NE&Esubmission/resubmission/final/scripts/F1/")
-
-# Makes Figure 1 panel B - size distributions
-source("panel-b-size-distributions.R")
-# Makes Figure 1 panel C - cell aspect ratio
-source("panel-c-aspect-ratio.R")
